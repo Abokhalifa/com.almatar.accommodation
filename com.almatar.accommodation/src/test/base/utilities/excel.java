b0cba1cb7af7c82fbc08e7cc453a06d856e6fa98
Abokhalifa <ahmed.abokhalifasoftxpert@gmail.com>
2019-08-31 10:47:13 +0200

    New folders

---
+++ com.almatar.accommodation/src/test/base/utilities/excel.java
@@ -0,0 +1,89 @@
+package utilities;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class excel {
+	
+	public static FileInputStream fi; 
+	public static FileOutputStream fo; 
+	public static XSSFWorkbook wb; 
+	public static XSSFSheet ws; 
+	public static XSSFRow row;
+	public static XSSFCell cell; 
+	
+	public static int getRowCount(String xlFile, String xlSheet) throws IOException
+	{
+		System.out.println("Good Evening!!");
+		fi = new FileInputStream(xlFile);
+		wb = new XSSFWorkbook(fi);
+		ws = wb.getSheet(xlSheet);
+		int rowCount =ws.getLastRowNum();
+		wb.close();	
+		fi.close();
+		return rowCount;
+	}
+	public static int getCellCount(String xlFile, String xlSheet, int rowNum) throws IOException
+	{
+		fi = new FileInputStream(xlFile);
+		wb = new XSSFWorkbook(fi);
+		ws = wb.getSheet(xlSheet);
+		row = ws.getRow(rowNum);
+		int cellCount =row.getLastCellNum();
+		wb.close();	
+		fi.close();
+		return cellCount;
+	}
+	
+	public static String getCellData(String xlFile, String xlSheet, int rowNum,int column) throws IOException
+	{
+		fi = new FileInputStream(xlFile);
+		wb = new XSSFWorkbook(fi);
+		ws = wb.getSheet(xlSheet);
+		row = ws.getRow(rowNum);
+		cell = row.getCell(column);
+		String data;
+		
+		try
+		{
+			DataFormatter formatter = new DataFormatter();
+			String cellData = formatter.formatCellValue(cell);
+			return cellData;
+		}
+		
+		catch(Exception e)
+		{
+			data = "";
+		}
+		
+		wb.close();	
+		fi.close();
+		return data;
+	}
+	
+	public static void setCellData(String xlFile, String xlSheet, int rowNum,int column,String data) throws IOException
+	{
+		fi = new FileInputStream(xlFile);
+		wb = new XSSFWorkbook(fi);
+		ws = wb.getSheet(xlSheet);
+		row = ws.getRow(rowNum);
+		cell =row.createCell(column);
+		cell.setCellValue(data);
+		fo=new FileOutputStream(xlFile);
+		wb.write(fo);
+		wb.close();	
+		fi.close();
+		fo.close();
+	}
+	
+	
+
+
+}
